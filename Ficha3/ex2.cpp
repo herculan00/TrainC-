09ec8a61e79bb76f1bf203e97f7b9b6587f94508
herculan00 <94049537+herculan00@users.noreply.github.com>
2024-02-18 12:47:19 +0000

    new-ex2

--- Ficha3/ex2.cpp
+++ Ficha3/ex2.cpp
@@ -3,14 +3,21 @@
 
 #include <iostream>
 #include "Ponto.h"
+#include "Triangulo.h"
 
 using namespace std;
 
 int main()
 {
-    Ponto a(1, 0), b(2, 0);
+    Ponto a(1, 0), b(2, 0); 
 
+    Ponto pontos[3]{ {1,3}, {2, 4}, {5, 7} };
 
-    cout << a.distancia(b)<<endl;
+    Triangulo t1("t1", a, b, pontos[0]);
+    Triangulo t2("t2", a, b, pontos[1]);
+    
+
+    cout << "Qqr letra para continuar..." << endl;
+    cin.ignore();
 }
 
